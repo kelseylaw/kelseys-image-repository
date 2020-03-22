70f99f43bfe698ae444ca423b780d49189f7ec9d
Kelsey Law <kelseyhlaw@gmail.com>
2020-03-21 22:19:15 -0700

    Added bcrypt, removed admin column from Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :images
+    validates :username, presence: true, length: { minimum: 2 }, uniqueness: { case_sensitive: false }
+
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :images
   resources :users
   root 'pages#home'
+  get 'register', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
