f27e8ad632db5afc60a4890c72b48653e085826e
Kelsey Law <kelseyhlaw@gmail.com>
2020-03-21 23:09:09 -0700

    Fixed Users page, updated Home page text

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   resources :images
   resources :users
-  root 'pages#home'
   get 'register', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
